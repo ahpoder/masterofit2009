4c6eedd1e7ee85103d79b9ae6eae1c0dc96c138c
ahpoder <ahpoder@f5114606-f668-11dd-9d1a-b59b2e1864b6>
2010-03-06 08:29:55 +0000

    Adding solution to obligatorisk 3 opg2

--- xml_and_Web/xmltek/obligatorisk3/opg2/src/JDOMSolution.java
+++ xml_and_Web/xmltek/obligatorisk3/opg2/src/JDOMSolution.java
@@ -26,41 +26,28 @@ public class JDOMSolution {
 	}
 
 	private void run() throws JDOMException, IOException {
+		long start = System.currentTimeMillis();
+		long startMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
 		SAXBuilder sx = new SAXBuilder();
-		Document d = sx.build(new File("recipes.xml"));
-		Namespace rcp = Namespace.getNamespace("http://www.brics.dk/ixwt/recipes");
+		Document d = sx.build(new File("handin3.xml"));
+		long parse = System.currentTimeMillis();
+		Namespace rcp = Namespace.getNamespace("http://cs.au.dk/A");
 		
-		Document d_mutant = sx.build(new File("recipes_mutant.xml"));
+		Filter f = new ElementFilter("foo", rcp);
 		
-		Filter f = new ElementFilter("recipe", rcp);
-
-		Document d_output = new Document();
-		
-		Element recipes = new Element("collection", "rcp", "http://www.brics.dk/ixwt/recipes");
-		d_output.addContent(recipes);
-		Element description = new Element("description", "rcp", "http://www.brics.dk/ixwt/recipes");
-		recipes.addContent(description);
-		description.addContent("This is a combined collection");
-
-		ArrayList<String> printed = new ArrayList<String>();
 		Iterator i = d.getDescendants(f);
 		while (i.hasNext())
 		{
 			Element e = (Element)i.next();
-			recipes.addContent((Element)e.clone());
-			printed.add(e.getAttributeValue("id"));
+			System.out.print(e.getText());
 		}
-		
-		Iterator i_mutant = d_mutant.getDescendants(f);
-		while (i_mutant.hasNext())
-		{
-			Element e_mutant = (Element)i_mutant.next();
-			if (!printed.contains(e_mutant.getAttributeValue("id")))
-			{
-				recipes.addContent((Element)e_mutant.clone());
-			}
-		}
-		XMLOutputter xo = new XMLOutputter();
-		xo.output(d_output, new FileOutputStream("combinedRecipes.xml"));
+		long end = System.currentTimeMillis();
+		long endMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+
+		System.out.println();
+		System.out.println();
+		System.out.println("Total time elapsed[ms]: " + (end - start));
+		System.out.println("Total spent parsing[ms]: " + (parse - start));
+		System.out.println("Total memory used[byte]: " + (endMemory - startMemory));
 	}
 }
--- xml_and_Web/xmltek/obligatorisk3/opg2/src/SAXSolution.java
+++ xml_and_Web/xmltek/obligatorisk3/opg2/src/SAXSolution.java
@@ -1,5 +1,13 @@
+import java.io.File;
 import java.io.IOException;
+import java.util.Iterator;
 
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.Namespace;
+import org.jdom.filter.ElementFilter;
+import org.jdom.filter.Filter;
+import org.jdom.input.SAXBuilder;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
@@ -23,17 +31,53 @@ public class SAXSolution {
 	}
 
 	private class AncestryHandler extends DefaultHandler {
+		boolean insideFooElement = false;
+		long _maxMemoryUsed = 0;
 		public void startElement(String uri, String localName, String qName, Attributes atts) {
-			
+			// TODO we only want to print the text that is directly inside foo, not the text that is 
+			// inside an element inside foo
+			insideFooElement = uri.equals("http://cs.au.dk/A") && localName.equals("foo");
+		}
+		
+		public void endElement(String uri, String localName, String qName)
+		{
+			insideFooElement = false;
+		}
+		public void characters(char[] ch, int start, int length)
+		{
+			long temp = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+			if (temp > _maxMemoryUsed)
+			{
+				_maxMemoryUsed = temp;
+			}
+			if (insideFooElement)
+			{
+				System.out.print(new String(ch, start, length));
+			}
 		}
 
+		public long maxMemoryUsed() {
+			return _maxMemoryUsed;
+		}
 	}
 	
 	private void run() throws SAXException, IOException {
+		long start = System.currentTimeMillis();
+		long startMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+
 		AncestryHandler handler = new AncestryHandler();
 		XMLReader reader = XMLReaderFactory.createXMLReader();
 		
 		reader.setContentHandler(handler);
-		reader.parse("recipes.xml");
+		reader.parse("handin3.xml");
+		
+		long end = System.currentTimeMillis();
+		
+		long endMemory = handler.maxMemoryUsed();
+		
+		System.out.println();
+		System.out.println();
+		System.out.println("Total time elapsed[ms]: " + (end - start));
+		System.out.println("Total memory used[byte]: " + (endMemory - startMemory));
 	}
 }
