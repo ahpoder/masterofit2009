d97d6015ba6a54ddd23c8cb851a9b8083340ed88
pmdster <pmdster@f5114606-f668-11dd-9d1a-b59b2e1864b6>
2010-04-11 09:48:02 +0000

    Temporary state

--- xml_and_Web/webtek/seminar1/opg6/src/SimpleClient.java
+++ xml_and_Web/webtek/seminar1/opg6/src/SimpleClient.java
@@ -4,14 +4,15 @@ import java.io.*;
 public class SimpleClient {
   public static void main(String[] args) {
     if (args.length!=3) {
-        System.out.println("Usage: java SimpleClient <host> <port>");
+        System.out.println("Usage: java SimpleClient <host>");
         System.exit(-1);
       }
-	  
+    Socket con = null;
+    FileOutputStream fo = null;
     try {
       String host = args[0];
-      Integer port = Integer.parseInt(args[1]);
-      Socket con = new Socket(host, port);
+      Integer port = 80;
+      con = new Socket(host, port);
 
       //Demo på seminar var kopieret fra bogens ImFeelingLucky eksempel
       //BufferedReader forventer text så ikke god til billeder. Laver om på bitstrengen. Brug InputStream og OutputStream
@@ -20,14 +21,18 @@ public class SimpleClient {
       out.write(0); // mark end of message (signal to the server)
       out.flush();
       
+      fo = new FileOutputStream("response.out");
       InputStreamReader in = new InputStreamReader(con.getInputStream());
       int c;
       while ((c = in.read())!=-1)
-        System.out.print((char)c);
+        fo.write(c);// System.out.print((char)c);
       
-      con.close();
     } catch (IOException e) {
       System.err.println(e); 
+    }
+    finally{
+        con.close();
+        fo.close();
     }
   }
 }
