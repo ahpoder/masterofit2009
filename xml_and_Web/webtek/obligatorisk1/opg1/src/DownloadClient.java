0eaad74a99c2d36075ea8117325ab9697e8a6a64
pmdster <pmdster@f5114606-f668-11dd-9d1a-b59b2e1864b6>
2010-04-15 21:11:54 +0000

    We've got the 301 thing handled now

--- xml_and_Web/webtek/obligatorisk1/opg1/src/DownloadClient.java
+++ xml_and_Web/webtek/obligatorisk1/opg1/src/DownloadClient.java
@@ -1,71 +1,81 @@
-import java.util.*;
+//import java.util.*;
 import java.net.*;
 import java.io.*;
 
 public class DownloadClient {
   public static void main(String[] args) {
 	//Added some help to remember the order of arguments (if more than one)
-    if (args.length!=1) {
-        System.out.println("Usage: java SimpleClient <host>");
+    if (args.length!=2) {
+        System.out.println("Usage: java SimpleClient <host> <port>");
         System.exit(-1);
       }
     try {
       String host = args[0];
-      //We default the request to be on port 80
-      Integer port = 80;
-      //Set up the connection
-      Socket con = new Socket(host, port);
-      
-      //Send the request on the connection output stream (based on IXVT p. 375)
-      BufferedWriter out =
-    	  new BufferedWriter
-    	     (new OutputStreamWriter(con.getOutputStream(), "UTF8"));
-      //We're interested in the host root only
-      out.write("GET / HTTP/1.1\r\n"); 
-      //The Host header is required (at least at the tested site: www.brics.dk)
-      out.write("Host: "+host+"\r\n"); 
-      //This one is merely to inform the server. It is NOT required for the request to be valid (on the server tested)
-      out.write("User-Agent: IXVT\r\n");
-      //Signal GET request termination with an empty line. This one is required for the request to be valid.
-      //If it is missing we will be waiting 'forever' for a server response
-      out.write("\r\n"); 
-      out.flush();
-      
-      //Get a handle on the server response      
-      InputStreamReader in = new InputStreamReader(con.getInputStream());
-      //This one's for easy line reading
-      BufferedReader inb = new BufferedReader( in );
-      
-      //The response status is in the first line
-      String responseStatus = inb.readLine();
-      
-      //The response header follows - read and store it
-      HashMap<String, String> header = new HashMap<String, String>();      
-      String line;      
-      while((line = inb.readLine()) != ""){
-      	//We reached the terminating empty line. Get out of here.
-      	if(line.isEmpty())
-      		break;
-      	//Add the (header, value) entry to the hash map
-      	header.put( line.substring(0, line.indexOf(":")).trim(), line.substring(line.indexOf(":")+1).trim());
-      }
+      Integer port = Integer.parseInt(args[1]);
 
-      //TODO: Analyze the response status to handle a '301 Moved Permanently'
+      Socket con = null;
+      InputStreamReader in = null;
+      String[] header = null;
+      int c;
+      int counter = 0;
+      //While loop to follow redirects
+      while(true){
+    	  counter++;
+          //Set up the connection
+          con = new Socket(host, port);
+          //Send the request on the connection output stream (based on IXVT p. 375)
+          BufferedWriter out =
+        	  new BufferedWriter
+        	     (new OutputStreamWriter(con.getOutputStream(), "UTF8"));
+          //We're interested in the host root only
+          out.write("GET / HTTP/1.1\r\n"); 
+          //The Host header is required (at least at the tested site: www.brics.dk)
+          out.write("Host: "+host+"\r\n"); 
+          //This one is merely to inform the server. It is NOT required for the request to be valid (on the server tested)
+          out.write("User-Agent: IXVT\r\n");
+          //Signal GET request termination with an empty line. This one is required for the request to be valid.
+          //If it is missing we will be waiting 'forever' for a server response
+          out.write("\r\n"); 
+          out.flush();
+          
+          //Get a stream on the server response      
+          in = new InputStreamReader(con.getInputStream());
+          
+          //Read the header into a string buffer. We're done when having reached an empty line (\r\n\r\n)
+          //We've avoided buffered read in order to support images etc. Mixing buffered read with character reads
+          //will fail, as the buffered reader will read ahead.
+          StringBuffer sbuf = new StringBuffer();
+          while ( ( c = in.read() ) != -1 ){
+        	  sbuf.append( (char)c );
+        	  if(sbuf.indexOf( "\r\n\r\n" ) > -1 )
+        		  break;
+          }
+          //Split the header at line breaks
+          header = sbuf.toString().split("[\r\n]{2}");
+        	  
+          //Analyze the response status to handle a '301 Moved Permanently'
+          if(header[0].contains("301")){
+        	  //Find and extract the location
+        	  for(int i = 0; i < header.length; i++)
+        		  if(header[i].indexOf("Location:") > -1)
+        			  host = header[i].substring(header[i].indexOf(":")+1).trim();
+          }
+          else
+        	  //Other response - get out of here
+        	  break;
+          //This is supposed to be a sefety valve for multiple 301 responses
+          //if(counter > 3)
+          //	break;      
+      }
       
       //Write the header to a file
       PrintWriter pw = new PrintWriter("header.out");
-      pw.printf("%1$s\r\n", responseStatus);
-      Set<String> set = header.keySet();
-      Iterator<String> iter = set.iterator();
-      while( iter.hasNext()){
-    	  String key = (String)iter.next();
-    	  pw.printf("%1$s:%2$s\r\n", key, (String)header.get(key));
-      }
+	  for(int i = 0; i < header.length; i++)
+		  pw.printf("%1$s\r\n", header[i]);
       pw.close();
       
-      //Pipe the response body into body.out 
+      //Pipe the response body into body.out use the InputStreamReader for this purpose 
       FileOutputStream fo = new FileOutputStream("body.out");
-      int c;
       while ((c = in.read())!=-1)
         fo.write(c);
       fo.close();
