ff70665028c27d26890828cf398b8e6de3f58e00
ahpoder <ahpoder@f5114606-f668-11dd-9d1a-b59b2e1864b6>
2010-04-17 05:11:43 +0000

    Updating obligatorisk 2 with the post part - opg2.txt description missing along with some code comments.

--- xml_and_Web/webtek/obligatorisk1/opg2/src/Guestbook.java
+++ xml_and_Web/webtek/obligatorisk1/opg2/src/Guestbook.java
@@ -25,45 +25,12 @@ public class Guestbook extends HttpServlet {
 	PrintWriter out = response.getWriter();
 	out.println("<html><head><title>Number guessing game</title></head><body>");
 
-	try
-	{
-		String xmlPath = getInitParameter("XMLPath");
-		File f = new File(xmlPath);
-		SAXBuilder sx = new SAXBuilder();
-		Document d;
-		if (f.exists())
-		{
-		  d = sx.build(f);
-		}
-		else
-		{
-		  d = new Document();
-		  Element root = new Element("entries");
-		  d.setRootElement(root);
-		}
+	String xmlPath = getInitParameter("XMLPath");
+	try {
+		Document d = loadXML(xmlPath);
 
 		Element entries = d.getRootElement();
 		
-		String entryName = request.getParameter("EntryName");
-		String entryText = request.getParameter("EntryText");
-		if (entryName != null)
-		{
-		  Element entry = new Element("entry");
-		  Element name = new Element("name");
-		  Element text = new Element("text");
-		  name.setText(entryName);
-		  text.setText(entryText);
-		  entry.addContent(name);
-		  entry.addContent(text);
-		  entries.addContent(entry);
-		  
-		  XMLOutputter xout = new XMLOutputter();
-		  java.io.FileWriter writer = new java.io.FileWriter(xmlPath);
-		  xout.output(d, writer);
-		  writer.flush();
-		  writer.close();
-		}
-
 		out.println("<table border=1><tr><th>Name</th><th>Text</th></tr>");
 		List entriesList =  entries.getChildren();
 		Iterator i = entriesList.iterator();
@@ -75,29 +42,80 @@ public class Guestbook extends HttpServlet {
 			out.println("<tr><td>" + name + "</td><td>" + text + "</td></tr>");
 		}
 		out.println("</table>");
-    }
-	catch (JDOMException ex)
-	{
-		out.println("JDOM exception occured<br>");
+	
+		out.println("<br>");
+		
+		String url = request.getRequestURI();
+		out.println("<form method=post action=\"" + response.encodeURL(url) + "\">");
+		out.println("Please enter the new entry name: ");
+		out.println("<input name=EntryName type=text><br>");
+		out.println("Please enter the new entry text: ");
+		out.println("<input name=EntryText type=text><br>");
+		out.println("<input type=submit name=submit value=\"New entry\">");
+		out.println("<br><br>");
+		out.println("</form>");
 	}
-	out.println("<br>");
-    
-	String url = request.getRequestURI();
-	out.println("<form method=get action=\"" + response.encodeURL(url) + "\">");
-	out.println("Please enter the new entry name: ");
-	out.println("<input name=EntryName type=text><br>");
-	out.println("Please enter the new entry text: ");
-	out.println("<input name=EntryText type=text><br>");
-	out.println("<input type=submit name=submit value=\"New entry\">");
-	out.println("<br><br>");
-	out.println("</form>");
-	out.println("<form method=post action=\"" + response.encodeURL(url) + "\">");
-	out.println("<input type=submit name=submit value=\"Post back\">");
-	out.println("</form>");
+	catch (JDOMException ex) {
+		out.println("<h2>FAILURE PROCESSING XML FILE - SITE DOWN</h2>");
+	}
+	out.println("</body></html>");
   }
 
+  private Document loadXML(String xmlPath) throws JDOMException, IOException
+  {
+	File f = new File(xmlPath);
+	SAXBuilder sx = new SAXBuilder();
+	Document d;
+	if (f.exists())
+	{
+	  d = sx.build(f);
+	}
+	else
+	{
+	  d = new Document();
+	  Element root = new Element("entries");
+	  d.setRootElement(root);
+	}
+	return d;
+  }
+  
   public void doPost(HttpServletRequest request,
                     HttpServletResponse response)
       throws IOException, ServletException {
+	String entryName = request.getParameter("EntryName");
+	String entryText = request.getParameter("EntryText");
+	
+	String xmlPath = getInitParameter("XMLPath");
+	try {
+		Document d = loadXML(xmlPath);
+		
+		Element entries = d.getRootElement();
+
+		// As the "only" way to post is to send these values (other than malicious posting, 
+		// and we do not need to protect against that in this type of app.
+		Element entry = new Element("entry");
+		Element name = new Element("name");
+		Element text = new Element("text");
+		name.setText(entryName);
+		text.setText(entryText);
+		entry.addContent(name);
+		entry.addContent(text);
+		entries.addContent(entry);
+
+		XMLOutputter xout = new XMLOutputter();
+		java.io.FileWriter writer = new java.io.FileWriter(xmlPath);
+		xout.output(d, writer);
+		writer.flush();
+		writer.close();
+		
+		doGet(request, response);
+	}
+	catch (JDOMException ex) {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html><head><title>Number guessing game</title></head><body>");
+		out.println("<h2>FAILURE PROCESSING XML FILE - SITE DOWN</h2>");
+		out.println("</body></html>");
+	}
   }
 }
