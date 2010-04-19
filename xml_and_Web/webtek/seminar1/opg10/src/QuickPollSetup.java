1e0267e10bd2a6241fcb8ac01e93d743ee053fb5
pmdster <pmdster@f5114606-f668-11dd-9d1a-b59b2e1864b6>
2010-04-19 19:44:48 +0000

    Added htmlEscape to the question before it is consumed

--- xml_and_Web/webtek/seminar1/opg10/src/QuickPollSetup.java
+++ xml_and_Web/webtek/seminar1/opg10/src/QuickPollSetup.java
@@ -6,7 +6,8 @@ public class QuickPollSetup extends HttpServlet {
   public void doPost(HttpServletRequest request, 
                      HttpServletResponse response)
       throws IOException, ServletException {
-    String q = request.getParameter("question");
+	//Part of solution to exercise b
+	String q = htmlEscape( request.getParameter("question") );
     ServletContext c = getServletContext();
     c.setAttribute("question", q);
     c.setAttribute("yes", new Integer(0));
@@ -19,4 +20,22 @@ public class QuickPollSetup extends HttpServlet {
               "Let the vote begin!"+
               "</body></html>");
   }
+  
+  //Part of solution to exercise b
+  private String htmlEscape(String s) {
+	    StringBuffer b = new StringBuffer();
+		for (int i = 0; i < s.length(); ++i) {
+		  char c = s.charAt(i);
+		  switch (c) {
+		    case '<': b.append("&lt;"); break;
+		    case '>': b.append("&gt;"); break;
+		    case '"': b.append("&quot;"); break;
+		    case '\'': b.append("&apos;"); break;
+		    case '&': b.append("&amp;"); break;
+			default: b.append(c);
+		  }
+		}
+		return b.toString();
+	  }
+  
 }
