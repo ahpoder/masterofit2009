a4dc031f4db66bd661dc8348f2cb08525cc836b2
ahpoder <ahpoder@f5114606-f668-11dd-9d1a-b59b2e1864b6>
2010-05-12 19:36:26 +0000

    Adding beginning of obligatorisk 3

---
+++ xml_and_Web/webtek/obligatorisk3/compile.bat
@@ -0,0 +1,2 @@
+javac -classpath "C:\Programmer\Apache Software Foundation\Tomcat 6.0\lib\servlet-api.jar;tomcat\RecipeServer\WEB-INF\lib\jdom.jar" src\*.java
+pause
---
+++ xml_and_Web/webtek/obligatorisk3/src/ChatServer.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.util.Random;
+import org.jdom.*;
+import org.jdom.input.*;
+import org.jdom.output.*;
+import org.jdom.xpath.*;
+import java.util.*;
+import java.net.*;
+
+public class CharServer extends HttpServlet {
+  public void doGet(HttpServletRequest request,
+                    HttpServletResponse response)
+      throws IOException, ServletException {
+    
+  }
+  
+  public void doPost(HttpServletRequest request,
+                    HttpServletResponse response)
+      throws IOException, ServletException {
+
+  }
+}
