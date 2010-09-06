57b969546ff1f74cb47e3b5cec1a6d2f1db2c1f8
pmdster <pmdster@f5114606-f668-11dd-9d1a-b59b2e1864b6>
2010-09-06 21:48:44 +0000

    Framework for the web-service created. Ant build management set up and tested. See readme.txt for steps to get started.

---
+++ xml_and_Web/webproj/pawebservice/src/ZoneServlet.java
@@ -0,0 +1,68 @@
+/*
+   This servlet illustrates the differences between
+   various scope levels i Servlets.
+
+   Open this servlet in two different browser windows
+   and reload their contents repeatedly. Explain how
+   and why the different numbers change their values.
+*/
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.util.Random;
+import org.jdom.*;
+import org.jdom.input.*;
+import org.jdom.output.*;
+import org.jdom.xpath.*;
+import java.util.*;
+import java.net.*;
+
+public class ZoneServlet extends HttpServlet {
+
+  public void doGet(HttpServletRequest request,
+                    HttpServletResponse response)
+      throws IOException, ServletException {
+
+	simpleResponse( "You called the ZoneServlet GET handler",
+					response);
+	return;
+  }
+
+  public void doPost(HttpServletRequest request,
+                    HttpServletResponse response)
+      throws IOException, ServletException {
+
+	simpleResponse( "You called the ZoneServlet POST handler",
+					response);
+	return;
+  }
+
+   public void doPut(HttpServletRequest request,
+                      HttpServletResponse response)
+      throws IOException, ServletException {
+
+	simpleResponse( "You called the ZoneServlet PUT handler",
+					response);
+	return;
+   }
+
+   private void simpleResponse( String title,
+   								HttpServletResponse response )
+   		throws IOException, ServletException{
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>geolog</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>");
+		out.println( title );
+		out.println("</h1>");
+		out.println("</body>");
+		out.println("</html>");
+		return;
+   }
+}
