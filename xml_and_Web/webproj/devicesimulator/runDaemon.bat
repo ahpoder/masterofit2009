8e576adf2db659b19e536b81cde878a18c707340
ahpoder <ahpoder@f5114606-f668-11dd-9d1a-b59b2e1864b6>
2010-09-13 19:55:53 +0000

    Updating batch files

--- xml_and_Web/webproj/devicesimulator/runDaemon.bat
+++ xml_and_Web/webproj/devicesimulator/runDaemon.bat
@@ -1,1 +1,3 @@
-java pa.geolog.DeviceMain --host http://localhost:8080 --status OK --id 10-20 --lattitude 54.1234 --longitude 9.42232 --deviation 1.02 --interval 60
+cd build
+java -classpath .;jdom.jar pa.geolog.DeviceMain --host http://localhost:8080 --status OK --id 10-20 --lattitude 54.1234 --longitude 9.42232 --deviation 1.02 --interval 60
+cd ..
