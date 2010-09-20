1fab526ed9b3a5a8ee827fa363937f7b148ec9b7
pmdster <pmdster@f5114606-f668-11dd-9d1a-b59b2e1864b6>
2010-09-20 07:00:45 +0000

    Changed id of device with error Added POST for a DISCONNECTED and DEPRECATED device

--- xml_and_Web/webproj/devicesimulator/runError.bat
+++ xml_and_Web/webproj/devicesimulator/runError.bat
@@ -1,3 +1,5 @@
 cd build
-java -classpath .;jdom.jar pa.geolog.DeviceMain --host http://localhost:8080 --status ERROR --id 27 --lattitude 54.1234 --longitude 9.42232
+java -classpath .;jdom.jar pa.geolog.DeviceMain --host http://localhost:8080 --status ERROR --id 28 --lattitude 54.2234 --longitude 9.52232
+java -classpath .;jdom.jar pa.geolog.DeviceMain --host http://localhost:8080 --status DISCONNECTED --id 29 --lattitude 54.4234 --longitude 9.72232
+java -classpath .;jdom.jar pa.geolog.DeviceMain --host http://localhost:8080 --status DEPRECATED --id 30 --lattitude 54.6234 --longitude 9.92232
 cd ..
