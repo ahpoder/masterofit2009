fd5a967cf74e610c2e9e9056b1c6a07631324fab
ahpoder@gmail.com <ahpoder@gmail.com@f5114606-f668-11dd-9d1a-b59b2e1864b6>
2013-09-22 19:38:14 +0000

    Minor updates to ER diagram and beginning updating the MDD (SQL). This seems to uncover some problems with the ER diagram along the way.

--- DatabaseDesign/DatabaseManagementSystems/miniproject_mdd.sql
+++ DatabaseDesign/DatabaseManagementSystems/miniproject_mdd.sql
@@ -1,53 +1,115 @@
 CREATE DATABASE test1;
 \c test1
 
-CREATE TABLE producttype (
-tid INTEGER PRIMARY KEY,
-name VARCHAR(128) NOT NULL
-);
-
-CREATE TABLE productline (
-plid INTEGER PRIMARY KEY,
-name VARCHAR(128) NOT NULL
-);
+CREATE TYPE productattributetype AS ENUM ('string', 'integer', 'float');
 
 CREATE TABLE product (
 pid INTEGER PRIMARY KEY,
 name VARCHAR(128) NOT NULL,
-pType INTEGER NOT NULL,
-inStock INTEGER NOT NULL,
-year INTEGER NOT NULL,
-productLine INTEGER NOT NULL,
-gender CHAR(1) NOT NULL,
-listPrice REAL NOT NULL,
-FOREIGN KEY (pType) REFERENCES ProductType(tid),
-FOREIGN KEY (productLine) REFERENCES ProductLine(plid)
+instock INTEGER NOT NULL DEFAULT 0,
+weight INTEGER NOT NULL 
 );
+
+CREATE TABLE productattributes (
+name VARCHAR(128) PRIMARY KEY,
+type productattributetype NOT NULL
+);
+
+CREATE TABLE productattributerelations (
+product INTEGER NOT NULL,
+attributename VARCHAR(128) NOT NULL,
+value VARCHAR(128) NOT NULL,
+PRIMARY KEY (product, attributename),
+FOREIGN KEY (product) REFERENCES product(pid),
+FOREIGN KEY (attributename) REFERENCES productattributes(name)
+);
+
+CREATE TYPE currency AS ENUM ('DKK', 'EUR', 'USD', 'GBP', 'AUD', 'INR', 'AED', 'CAD', 'CHF', 'CNY');
 
 CREATE TABLE manufactorer (
-mid INTEGER PRIMARY KEY,
+vatno VARCHAR(128) PRIMARY KEY,
 name VARCHAR(128) NOT NULL
+paymentcurrency currency NOT NULL,
+invoiceaddress VARCHAR(256)
 );
 
-CREATE TABLE production (
-product INTEGER NOT NULL,
-manufactorer INTEGER NOT NULL,
-price REAL NOT NULL,
-PRIMARY KEY (product,manufactorer),
-FOREIGN KEY (product) REFERENCES product(pid),
-FOREIGN KEY (manufactorer) REFERENCES manufactorer(mid)
+-- As we generate the order id we could have it be globally unique, and then have it be the sole primary key. Makes it easier later?
+CREATE TABLE manufactorerorders (
+manufactorerid VARCHAR(128), 
+orderid INTEGER,
+orderdate DATETIME NOT NULL,
+PRIMARY KEY(manufactorerid,orderid),
+FOREIGN KEY(manufactorerid) REFERENCES manufactorer(vatno)
 );
 
-CREATE TABLE purchases (
+-- The manufactorerid and orderid is unique, but so is manufactorerid and cocno (unless the manufactorer restarts the number)
+-- This also does not match perfectly with the ER diagram, but it is easier as we do not have a guarantee that the orderid is globally unique.
+CREATE TABLE manufactorerorderconfirmations (
+manufactorerid VARCHAR(128), 
+orderid INTEGER,
+cocno VARCHAR(128) NOT NULL,
+PRIMARY KEY(manufactorerid,orderid),
+UNIQUE (orderid, cocno),
+UNIQUE (manufactorerid, cocno),
+FOREIGN KEY(manufactorerid) REFERENCES manufactorer(vatno),
+FOREIGN KEY(orderid) REFERENCES manufactorerorders(orderid)
+);
+
+CREATE TABLE manufactorerinvoices (
+manufactorerid VARCHAR(128), 
+orderid INTEGER,
+invoiceno VARCHAR(128) NOT NULL,
+paybefore DATE NOT NULL,
+paied BOOLEAN NOT NULL DEFAULT false,
+PRIMARY KEY(manufactorerid,orderid),
+UNIQUE (orderid, invoiceno),
+UNIQUE (manufactorerid, invoiceno),
+FOREIGN KEY(manufactorerid) REFERENCES manufactorer(vatno),
+FOREIGN KEY(orderid) REFERENCES manufactorerorders(orderid)
+);
+
+CREATE TABLE manufactorerdeliveries (
+manufactorerid VARCHAR(128), 
+orderid INTEGER,
+deliverydate DATETIME NOT NULL,
+PRIMARY KEY(manufactorerid,orderid),
+FOREIGN KEY(manufactorerid) REFERENCES manufactorer(vatno),
+FOREIGN KEY(orderid) REFERENCES manufactorerorders(orderid)
+);
+
+-- trigger for inserting the products in the warehouse???
+
+CREATE TABLE phones (
 pid INTEGER PRIMARY KEY,
-purchaseDate TIMESTAMP NOT NULL,
-product INTEGER NOT NULL,
-manufactorer INTEGER NOT NULL,
-purchasePrice REAL NOT NULL,
-count INTEGER NOT NULL,
-FOREIGN KEY (product) REFERENCES product(pid),
-FOREIGN KEY (manufactorer) REFERENCES manufactorer(mid)
+phone VARCHAR(32)
 );
+
+CREATE TYPE apartmentlocations AS ENUM ('left', 'middle', 'right');
+CREATE TYPE countries AS ENUM ('Denmark', 'England', 'USA');
+
+CREATE TABLE customers (
+wid INTEGER PRIMARY KEY,
+firstname VARCHAR(128) NOT NULL,
+middlename VARCHAR(128) NULL,
+sirname VARCHAR(128) NOT NULL,
+tvmfth apartmentlocations NULL,
+floor INTEGER NULL,
+streetletter CHAR(2) NULL,
+streetnumber INTEGER NOT NULL,
+streetname VARCHAR(128) NOT NULL,
+postalcode VARCHAR(32) NOT NULL,
+region VARCHAR(64) NULL,
+country countries NOT NULL,
+phones INTEGER NULL,
+FOREIGN KEY(phones) REFERENCES phones(pid)
+);
+
+CREATE TABLE webshops (
+
+);
+
+
+
 
 CREATE TABLE retailer (
 rid INTEGER PRIMARY KEY,
