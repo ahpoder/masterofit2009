83ce66ae32cb626f6a6ab028372fe1fa2cded47e
ahpoder@gmail.com <ahpoder@gmail.com@f5114606-f668-11dd-9d1a-b59b2e1864b6>
2013-09-24 19:32:03 +0000

    Updating ER diagram a little. Adding more tables and beginning an example query run.

--- DatabaseDesign/DatabaseManagementSystems/miniproject_mdd.sql
+++ DatabaseDesign/DatabaseManagementSystems/miniproject_mdd.sql
@@ -3,8 +3,9 @@ CREATE DATABASE test1;
 
 CREATE TYPE productattributetype AS ENUM ('string', 'integer', 'float');
 
+-- weight is in grams so integer is fine.
 CREATE TABLE product (
-pid INTEGER PRIMARY KEY,
+pid SERIAL PRIMARY KEY,
 name VARCHAR(128) NOT NULL,
 instock INTEGER NOT NULL DEFAULT 0,
 weight INTEGER NOT NULL 
@@ -24,20 +25,34 @@ FOREIGN KEY (product) REFERENCES product(pid),
 FOREIGN KEY (attributename) REFERENCES productattributes(name)
 );
 
+CREATE TYPE termsofdelivery AS ENUM ('abLager', 'SideOfShop', '3month', '14dg', '7dg', '1dg');
+
+CREATE SEQUENCE pricing_plan_seq;
+
+-- if the pricing plans are to be shared it is important that they are immutable (cannot be gauranteed by the DB)
+-- serial is a pseudonum for integer/big so it is possible to use an integer to refecerence it in a foreign key.
+-- should the pricing plan include a currency??? - or do we default to DKK?
+CREATE TABLE pricingplans (
+  id SERIAL PRIMARY KEY,
+  price FLOAT NOT NULL,
+  discount FLOAT NULL,
+  deliveryconditions termsofdelivery NULL
+);
+ALTER SEQUENCE user_id_seq OWNED BY user.user_id;
+
 CREATE TYPE currency AS ENUM ('DKK', 'EUR', 'USD', 'GBP', 'AUD', 'INR', 'AED', 'CAD', 'CHF', 'CNY');
 
 CREATE TABLE manufactorer (
 vatno VARCHAR(128) PRIMARY KEY,
 name VARCHAR(128) NOT NULL
-paymentcurrency currency NOT NULL,
-invoiceaddress VARCHAR(256)
+paymentcurrency currency NOT NULL
 );
 
 -- As we generate the order id we could have it be globally unique, and then have it be the sole primary key. Makes it easier later?
 CREATE TABLE manufactorerorders (
 manufactorerid VARCHAR(128), 
 orderid INTEGER,
-orderdate DATETIME NOT NULL,
+orderdate DATE NOT NULL,
 PRIMARY KEY(manufactorerid,orderid),
 FOREIGN KEY(manufactorerid) REFERENCES manufactorer(vatno)
 );
@@ -59,6 +74,7 @@ CREATE TABLE manufactorerinvoices (
 manufactorerid VARCHAR(128), 
 orderid INTEGER,
 invoiceno VARCHAR(128) NOT NULL,
+invoicedate DATE NOT NULL,
 paybefore DATE NOT NULL,
 paied BOOLEAN NOT NULL DEFAULT false,
 PRIMARY KEY(manufactorerid,orderid),
@@ -77,6 +93,29 @@ FOREIGN KEY(manufactorerid) REFERENCES manufactorer(vatno),
 FOREIGN KEY(orderid) REFERENCES manufactorerorders(orderid)
 );
 
+CREATE TABLE manufactorerorderedproducts (
+manufactorerid VARCHAR(128),
+orderid INTEGER,
+productid INTEGER,
+priceingplanid INTEGER NOT NULL,
+count INTEGER NOT NULL,
+PRIMARY KEY (manufactorerid,orderid,productid),
+FOREIGN KEY (manufactorerid) REFERENCES manufactorerorders(manufactorerid),
+FOREIGN KEY (orderid) REFERENCES manufactorerorders(orderid),
+FOREIGN KEY (productid) REFERENCES product(pid),
+FOREIGN KEY (priceingplanid) REFERENCES pricingplans(id)
+);
+
+CREATE TABLE manufactorerproducts (
+manufactorerid VARCHAR(128),
+productid INTEGER,
+priceingplanid INTEGER NOT NULL,
+PRIMARY KEY (manufactorerid,productid),
+FOREIGN KEY (manufactorerid) REFERENCES manufactorerorders(manufactorerid),
+FOREIGN KEY (productid) REFERENCES product(pid),
+FOREIGN KEY (priceingplanid) REFERENCES pricingplans(id)
+);
+
 -- trigger for inserting the products in the warehouse???
 
 CREATE TABLE phones (
@@ -86,9 +125,10 @@ phone VARCHAR(32)
 
 CREATE TYPE apartmentlocations AS ENUM ('left', 'middle', 'right');
 CREATE TYPE countries AS ENUM ('Denmark', 'England', 'USA');
+CREATE TYPE termsofpayment AS ENUM ('prepay', '10dgNet', '14dgNet', '30dgNet', 'LbMntPlus15dg');
 
 CREATE TABLE customers (
-wid INTEGER PRIMARY KEY,
+id INTEGER PRIMARY KEY,
 firstname VARCHAR(128) NOT NULL,
 middlename VARCHAR(128) NULL,
 sirname VARCHAR(128) NOT NULL,
@@ -101,96 +141,98 @@ postalcode VARCHAR(32) NOT NULL,
 region VARCHAR(64) NULL,
 country countries NOT NULL,
 phones INTEGER NULL,
+termsofpayment paymentconditions NOT NULL DEFAULT 'prepay'
 FOREIGN KEY(phones) REFERENCES phones(pid)
 );
 
+-- partly for fun and partly because we control the IDs we reverse the dependency and make the ids unique.
+
+CREATE TABLE netspayments (
+netsid INTEGER PRIMARY KEY, 
+transactionhash VARCHAR(2048)
+);
+
+-- This also does not match perfectly with the ER diagram, but it is easier as we do not have a guarantee that the orderid is globally unique.
+CREATE TABLE customerorderconfirmations (
+cocno INTEGER PRIMARY KEY,
+cocdate DATE NOT NULL
+);
+
+-- a null in paybefore is used for creditcard payments (immediate)
+CREATE TABLE customerinvoices (
+invoiceno INTEGER PRIMARY KEY,
+invoicedate DATE NOT NULL,
+paybefore DATE NULL,
+paied BOOLEAN NOT NULL DEFAULT false
+);
+
+-- freightno is null is the delivery is picked up at the warehouse
+CREATE TABLE customerdeliveries (
+deliveryid INTEGER PRIMARY KEY,
+deliverydate DATE NOT NULL,
+freightno VARCHAR(128) NULL,
+);
+
+-- the orderid can be left unique only in conjunction with the customer, but that makes the relationship to purchased products more complicated.
+CREATE TABLE customerorders (
+orderid INTEGER PRIMARY KEY,
+customerid INTEGER NOT NULL, 
+orderdate DATETIME NOT NULL,
+netsid INTEGER NULL,
+cocid INTEGER NULL,
+invoiceid INTEGER NULL,
+deliveryid INTEGER NULL,
+PRIMARY KEY(customerid,orderid),
+FOREIGN KEY(customerid) REFERENCES customers(id),
+FOREIGN KEY(netsid) REFERENCES netspayments(netsid),
+FOREIGN KEY(cocno) REFERENCES customerorderconfirmations(cocno),
+FOREIGN KEY(invoiceid) REFERENCES customerinvoices(invoiceno),
+FOREIGN KEY(deliveryid) REFERENCES customerdeliveries(deliveryid)
+);
+
 CREATE TABLE webshops (
-
+  id INTEGER PRIMARY KEY,
+  vatno VARCHAR(64) NOT NULL,
+  name VARCHAR(128) NOT NULL,
+  paymentcurrency currency NOT NULL,
+  invoiceaddress VARCHAR(256) NOT NULL,
+  paymentconditions termsofpayment NOT NULL DEFAULT '30dbNet'
 );
 
-
-
-
-CREATE TABLE retailer (
-rid INTEGER PRIMARY KEY,
-name VARCHAR(128) NOT NULL
+CREATE TABLE quantitydiscounts (
+  id INTEGER PRIMARY KEY,
+  count INTEGER NOT NULL,
+  discount FLOAT NOT NULL
 );
 
-CREATE TABLE retailercarries (
-retailer INTEGER NOT NULL,
-product INTEGER NOT NULL,
-price REAL NOT NULL,
-PRIMARY KEY (retailer,product),
-FOREIGN KEY (retailer) REFERENCES retailer(rid),
-FOREIGN KEY (product) REFERENCES product(pid)
-);
-
-CREATE TABLE retailersales (
-sid INTEGER PRIMARY KEY,
-orderId INTEGER NOT NULL,
-retailer INTEGER NOT NULL,
-salesDate TIMESTAMP NOT NULL,
-product INTEGER NOT NULL,
-purchasePrice REAL NOT NULL,
-count INTEGER NOT NULL,
-FOREIGN KEY (product) REFERENCES product(pid),
-FOREIGN KEY (retailer) REFERENCES retailer(rid)
-);
-
-CREATE TABLE webshop (
-wid INTEGER PRIMARY KEY,
-name VARCHAR(128) NOT NULL
+-- be very very careful. Inserting into this relation will change an existing pricing plan - can it be prevented?
+CREATE TABLE quantitydiscountrelations (
+  pricingplanid INTEGER,
+  discountid INTEGER,
+  PRIMARY KEY (pricingplanid, discountid),
+  FOREIGN KEY (pricingplanid) REFERENCES pricingplans(id),
+  FOREIGN KEY (discountid) REFERENCES quantitydiscounts(id)
 );
 
 CREATE TABLE webshopcarries (
-webshop INTEGER NOT NULL,
-product INTEGER NOT NULL,
-price REAL NOT NULL,
-PRIMARY KEY (webshop,product),
-FOREIGN KEY (webshop) REFERENCES webshop(wid),
-FOREIGN KEY (product) REFERENCES product(pid)
-);
-
-CREATE TABLE customer (
-cid INTEGER PRIMARY KEY,
-name VARCHAR(128) NOT NULL,
-authentication UUID NOT NULL,
-webshop INTEGER NOT NULL,
-discount INTEGER NOT NULL DEFAULT 0,
-FOREIGN KEY (webshop) REFERENCES webshop(wid)
-);
-
-CREATE TABLE webshopsales (
-sid INTEGER PRIMARY KEY,
-orderId INTEGER NOT NULL,
-customer INTEGER NOT NULL,
-salesDate TIMESTAMP NOT NULL,
-product INTEGER NOT NULL,
-purchasePrice REAL NOT NULL,
-count INTEGER NOT NULL,
+webshopid INTEGER NOT NULL,
+productid INTEGER NOT NULL,
+wpricingplanid INTEGER NOT NULL,
+ppricingplanid INTEGER NOT NULL,
+PRIMARY KEY (webshopid,productid),
+FOREIGN KEY (retailer) REFERENCES retailer(rid),
 FOREIGN KEY (product) REFERENCES product(pid),
-FOREIGN KEY (customer) REFERENCES customer(cid)
+FOREIGN KEY (wpricingplanid) REFERENCES pricingplans(id),
+FOREIGN KEY (ppricingplanid) REFERENCES pricingplans(id)
 );
 
-CREATE ROLE RetailerRole;
-CREATE ROLE ManufactorerRole;
-CREATE ROLE WebshopRole;
-CREATE ROLE CustomerRole;
-CREATE ROLE Wholesaler;
-
-GRANT SELECT ON webshop TO GROUP WebshopRole;
-GRANT SELECT, INSERT ON webshopsales TO GROUP WebshopRole;
-GRANT SELECT ON webshopsales TO GROUP CustomerRole;
-GRANT SELECT ON product TO GROUP WebshopRole;
-GRANT SELECT, INSERT ON webshopcarries TO GROUP WebshopRole;
-
-// Limit to column = GRANT SELECT (col1) ON webshopsales TO GROUP CustomerRole;
-
-GRANT SELECT ON retailer TO GROUP RetailerRole;
-GRANT SELECT ON retailersales TO GROUP RetailerRole;
-
-GRANT SELECT, INSERT ON webshopcarries TO GROUP RetailerRole;
-
-// Add triggers
-
-// We are unable to limit the quiry further. How is a good design for limiting the select to a specific where
+CREATE TABLE customerorderproducts (
+orderid INTEGER,
+productid INTEGER,
+priceingplanid INTEGER NOT NULL,
+count INTEGER NOT NULL,
+PRIMARY KEY (orderid,productid),
+FOREIGN KEY (orderid) REFERENCES customerorders(orderid),
+FOREIGN KEY (productid) REFERENCES product(pid),
+FOREIGN KEY (priceingplanid) REFERENCES pricingplans(id)
+);
--- DatabaseDesign/DatabaseManagementSystems/miniproject_queries.sql
+++ DatabaseDesign/DatabaseManagementSystems/miniproject_queries.sql
@@ -1,3 +1,42 @@
-SELECT * FROM webshopsales WHERE customer IN (SELECT cid FROM customer WHERE authentication=@authentication);
-SELECT * FROM webshopsales INNER JOIN customer ON webshopsales.customer=customer.cid WHERE authentication=@authentication;
-// Which is better??
+-- Be adviced that temporary tables are used to ensure that we have the auto generated IDs. Inpractice these IDs would exist in application and not in temporary tables.
+
+-- Create a product
+INSERT INTO product (name,weight) VALUES ("XB T-Shirt",156);
+CREATE TEMPORARY TABLE tempproductid (productid INTEGER NOT NULL);
+INSERT INTO tempproductid VALUES (LASTVAL());
+
+-- Create a manufactorer
+INSERT INTO manufactorer VALUES ('CN34554345','ChinaProductionExcelence','CNY');
+
+-- Create a pricing plan
+INSERT INTO pricingplans (price,discount,deliveryconditions) VALUES (129.56,0.0,'abLager');
+CREATE TEMPORARY TABLE tempmpricingplanid (ppid INTEGER NOT NULL);
+INSERT INTO tempmpricingplanid VALUES (LASTVAL());
+
+-- Create a manufactorer product
+INSERT INTO manufactorerproducts VALUES ('CN34554345',SELECT productid FROM tempproductid LIMIT 1,SELECT ppid FROM tempmpricingplanid LIMIT 1);
+
+-- Place order with manufactorer
+BEGIN TRANSACTION;
+INSERT INTO manufactorerorders (manufactorerid,orderdate) VALUES ('CN34554345',CURRENT_DATE); 
+-- This is a trick to the code can be executed in sequence, but in reality it would be an insert with a select of the id followed by another INSERT controlled by the application
+CREATE TEMPORARY TABLE temporderid (orderid INTEGER NOT NULL);
+INSERT INTO temporderid VALUES (LASTVAL());
+INSERT INTO manufactorerorderedproducts (manufactorerid,orderid,productid,priceingplanid,count) VALUES ('CN34554345',SELECT orderid FROM temporderid LIMIT 1,SELECT productid FROM tempproductid LIMIT 1,SELECT priceingplanid FROM manufactorerproducts WHERE manufactorerid='CN34554345' AND productid=(SELECT productid FROM tempproductid LIMIT 1),1200);
+END TRANSACTION;
+
+-- After the COC has been received it is verified manually and entered. The order id will be on the COC.
+INSERT INTO manufactorerorderconfirmations VALUES ('CN34554345',SELECT orderid FROM temporderid LIMIT 1,'CPE00012');
+
+-- After the invoice is received it is verified manually and entered. The order id will be on the invoice.
+-- The payment date may be calculated from the invoice date and the terms of payment, but we will postpone that for now.
+INSERT INTO manufactorerinvoices (manufactorerid,orderid,invoiceno,invoicedate,paybefore) VALUES ('CN34554345',SELECT orderid FROM temporderid LIMIT 1,'CPE-INV-00007','2013-10-16','2013-11-16');
+
+-- After the product is delivered
+-- The 1200 could be extracted from the manufactorerorderedproducts, but we have chosen to enter it directly, but naturally the delivery was confirmed by looking at manufactorerorderedproducts.
+BEGIN TRANSACTION;
+INSERT INTO manufactorerdeliveries VALUES ('CN34554345',SELECT orderid FROM temporderid LIMIT 1,'2013-10-30');
+UPDATE products SET instock=instock+1200 WHERE pid IN (SELECT productid FROM tempproductid LIMIT 1);
+END TRANSACTION;
+
+
